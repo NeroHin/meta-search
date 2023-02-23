c4f51b732a50d5d578a6245e17346ae7f5a827e7
NeroUcH <hin1997101@gmail.com>
2023-02-23 21:31:06 +0800

    feat: complete web search api func

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
+import http.client
+from duckduckgo_search import ddg
 import requests
 from typing import Optional
 import json
 from fastapi import FastAPI
-from duckduckgo_search import ddg 
+from duckduckgo_search import ddg
 
 # create a FastAPI instance
 app = FastAPI()
@@ -26,17 +28,57 @@ def get_top_ddg_result(keyword: Optional[str], top_number: int = 3):
     ddg_top = ddg_result[:top_number]
     ddg_top_title = []
     ddg_top_link = []
+    ddg_top_description = []
 
     for result in ddg_top:
         ddg_top_title.append(result['title'])
         ddg_top_link.append(result['href'])
+        ddg_top_description.append(result['body'])
 
-    return ddg_top_title, ddg_top_link
+    return ddg_top_title, ddg_top_link, ddg_top_description
+
+
+def get_top_web_search_result(keyword: Optional[str], top_number: int = 3):
+    '''
+        input: keyword, top_number
+        process: get top_number of web search result
+        output: top_number of web search result title and link
+
+    '''
+
+    conn = http.client.HTTPSConnection(
+        "contextualwebsearch-websearch-v1.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': "416132865dmshcd8a00c90d4dc07p13519ajsn32f2dc6b7525",
+        'X-RapidAPI-Host': "contextualwebsearch-websearch-v1.p.rapidapi.com"
+    }
+
+    conn.request(
+        "GET", f"/api/Search/WebSearchAPI?q={ keyword }&pageNumber=1&pageSize={ top_number }&autoCorrect=true", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    search_result = json.loads(data.decode("utf-8"))["value"]
+
+    web_search_result_title = []
+    web_search_result_link = []
+    web_search_result_description = []
+
+    for result in search_result:
+        web_search_result_title.append(result['title'])
+        web_search_result_link.append(result['url'])
+        web_search_result_description.append(result['description'])
+
+    return web_search_result_title, web_search_result_link, web_search_result_description
 
 
 @app.get('/search/{keyword}')
 def search(keyword: str):
 
-    ddg_top_title, ddg_top_link = get_top3_ddg_result(keyword)
+    ddg_top_title, ddg_top_link, ddg_top_description = get_top3_ddg_result(
+        keyword=keyword)
+    web_search_result_title, web_search_result_link, web_search_result_description = get_top3_web_search_result(
+        keyword=keyword)
 
-    return {'keyword': keyword, 'top3_title': ddg_top_title, 'top3_link': ddg_top_link}
+    return {'keyword': keyword, 'ddg_top_title': ddg_top_title, 'ddg_top_link': ddg_top_link, 'ddg_top_description': ddg_top_description, 'web_search_result_title': web_search_result_title, 'web_search_result_link': web_search_result_link, 'web_search_result_description': web_search_result_description}
