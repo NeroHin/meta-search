39458252fd7541278b3da0cd2318f71ffcfdf0e2
NeroUcH <hin1997101@gmail.com>
2023-02-23 23:02:44 +0800

    feat: complete google search func.

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from typing import Optional
 import json
 from fastapi import FastAPI
 import uvicorn
-
+import urllib.parse
 
 # create a FastAPI instance
 app = FastAPI()
@@ -24,7 +24,7 @@ def get_top_ddg_result(keyword: Optional[str], top_number: int = 3):
 
     '''
 
-    ddg_result = ddg(keywords=keyword, region="tw-tzh", safesearch='Off')
+    ddg_result = ddg(keywords=keyword, safesearch='Off', region="t")
 
     ddg_top = ddg_result[:top_number]
     ddg_top_title = []
@@ -74,18 +74,57 @@ def get_top_web_search_result(keyword: Optional[str], top_number: int = 3):
     return web_search_result_title, web_search_result_link, web_search_result_description
 
 
+def get_top_google_search_result(keyword: Optional[str], top_number: int = 3):
+
+    # get the API KEY here: https://developers.google.com/custom-search/v1/overview
+    API_KEY = "AIzaSyAEltei7_fvI0S1CIxyhg_4QMBgPzcdtlk"
+    # get your Search Engine ID on your CSE control panel
+    SEARCH_ENGINE_ID = "13c5e1a0875b947ba"
+
+    # using the first page
+    page = 1
+
+    url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={keyword}"
+
+    # make the API request
+    data = requests.get(url).json()
+
+    # get the result items
+    search_items = data.get("items")
+
+    # initialize the result lists
+    google_result_title = []
+    google_result_link = []
+    google_result_description = []
+
+    # select the title, link and snippet with top_number
+    for item in search_items[:top_number]:
+        google_result_title.append(item["title"])
+        google_result_link.append(item["link"])
+        google_result_description.append(item["snippet"].replace("\xa0", ""))
+
+    return google_result_title, google_result_link, google_result_description
 
 
 @app.get('/search/{keyword}')
 def search(keyword: str):
+    
+    # encode the keyword with utf-8
+    
+    keyword = urllib.parse.quote(keyword)
 
-    ddg_top_title, ddg_top_link, ddg_top_description = get_top_ddg_result(
+    ddg_result_title, ddg_result_link, ddg_result_description = get_top_ddg_result(
         keyword=keyword)
     web_search_result_title, web_search_result_link, web_search_result_description = get_top_web_search_result(
         keyword=keyword)
+    google_result_title, google_result_link, google_result_description = get_top_google_search_result(
+        keyword=keyword)
 
-    return {'keyword': keyword, 'ddg_top_title': ddg_top_title, 'ddg_top_link': ddg_top_link, 'ddg_top_description': ddg_top_description, 'web_search_result_title': web_search_result_title, 'web_search_result_link': web_search_result_link, 'web_search_result_description': web_search_result_description}
+    return {"duckduckgo": {"title": ddg_result_title, "link": ddg_result_link, "description": ddg_result_description},
+            "web_search": {"title": web_search_result_title, "link": web_search_result_link, "description": web_search_result_description},
+            "google_search": {"title": google_result_title, "link": google_result_link, "description": google_result_description}}
+
 
 if __name__ == '__main__':
 
-    uvicorn.run(app, host='0.0.0.0')
+    uvicorn.run(app)
