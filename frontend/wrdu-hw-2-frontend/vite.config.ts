92870cdaee120c3f2b067855597bf8ddc299da2e
NeroHin <hin1997101@gmail.com>
2023-02-27 03:58:22 +0800

    style: update web styling

--- frontend/wrdu-hw-2-frontend/vite.config.ts
+++ frontend/wrdu-hw-2-frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:8087',
+        changeOrigin: true,
+      },
+    },
+  },
 })
