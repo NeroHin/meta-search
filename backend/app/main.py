71613a74bec41455c7098d4848649774849f6d84
NeroHin <hin1997101@gmail.com>
2023-02-27 03:57:33 +0800

    feat: update CROS setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,14 +11,10 @@ import urllib.parse
 # create a FastAPI instance
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
