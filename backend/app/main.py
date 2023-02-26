a895bd6fca4511d278f7e725eaacdb4d6e1d506b
NeroHin <hin1997101@gmail.com>
2023-02-26 20:22:24 +0800

    feat: update CROS setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,25 @@ import requests
 from typing import Optional
 import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import urllib.parse
 
 # create a FastAPI instance
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def read_root():
